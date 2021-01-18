201c35e5e27ae6475e886d11947abf58bd3b8a81
Masafumi Endo <endoma@oregonstate.edu>
2021-01-17 16:16:32 -0800

    object-oriented A*

--- HW1/Astar/Astar.m
+++ HW1/Astar/Astar.m
@@ -5,35 +5,48 @@
 
 classdef Astar
     
+    % set properties
     properties
+        start
+        goal
+        map
+        h_type
     end
     
     methods
-        function [path] = search(obj, start, goal, map, h_type)
+        % constructor
+        function obj = Astar(start, goal, map, h_type)
+            obj.start = start;
+            obj.goal = goal;
+            obj.map = map;
+            obj.h_type = h_type;
+        end
+        
+        function [path] = search(obj)
             
             % initialization
-            [~, num_nodes] = get_start(map);
+            [~, num_nodes] = get_start(obj.map);
             eval_list = inf * ones(num_nodes, 1); % evaluation function list
             open_list = pq_init(num_nodes); % open list
             % starting position
-            eval_list(start) = 0;
-            open_list = pq_set(open_list, start, eval_list(start));
+            eval_list(obj.start) = 0;
+            open_list = pq_set(open_list, obj.start, eval_list(obj.start));
             while true
                 % pick best node and remove it from open_list
                 [open_list, n_best] = pq_pop(open_list);
                 % check n_best is goal or not
-                if n_best == goal
+                if n_best == obj.goal
                     break
                 end
                 
                 % expand all nodes that are neibhbors of n_best
                 % note that the last action is [0; 0] so the agent doesn't
                 % move
-                [neighbors, ~] = get_neighbors(map, n_best);
+                [neighbors, ~] = get_neighbors(obj.map, n_best);
                 % -1 is for removing the last action 
                 for index_neighbor=1:length(neighbors)-1
                     neighbor = neighbors(index_neighbor);
-                    eval = actual(obj, n_best, eval_list) + heuristic(obj, map, neighbor, h_type);
+                    eval = actual(obj, n_best, eval_list) + heuristic(obj, neighbor);
                     % check the current and previous cost
                     if eval < eval_list(neighbor)
                         eval_list(neighbor) = eval;
@@ -44,17 +57,17 @@ classdef Astar
             end
             
             % after finishing search process, get an optimal path from solution
-            path = get_path(obj, start, goal, map, eval_list);
+            path = get_path(obj, eval_list);
         end
         
-        function [path] = get_path(obj, start, goal, map, eval_list)
+        function [path] = get_path(obj, eval_list)
             % initialization
             path = [];
-            n_best = goal; % start from the goal node
-            while n_best ~= start
-                [x_best, y_best] = state_from_index(map, n_best);
+            n_best = obj.goal; % start from the goal node
+            while n_best ~= obj.start
+                [x_best, y_best] = state_from_index(obj.map, n_best);
                 path = [path; x_best, y_best];
-                [neighbors, ~] = get_neighbors(map, n_best);
+                [neighbors, ~] = get_neighbors(obj.map, n_best);
                 neighbors = neighbors(1:end-1);
                 [~, index_neighbor] = min(eval_list(neighbors));
                 % update n_best for the next loop
@@ -68,13 +81,13 @@ classdef Astar
         end
         
         % function to calculate heuristic cost of the neighbor
-        function [cost_h] = heuristic(obj, map, neighbor, h_type)
-            [x_neig, y_neig] = state_from_index(map, neighbor);
-            [x_goal, y_goal] = state_from_index(map, get_goal(map));
-            if h_type == 'm'
+        function [cost_h] = heuristic(obj, neighbor)
+            [x_neig, y_neig] = state_from_index(obj.map, neighbor);
+            [x_goal, y_goal] = state_from_index(obj.map, get_goal(obj.map));
+            if obj.h_type == 'm'
                 % manhattan distance
                 cost_h = abs(x_neig - x_goal) + abs(y_neig - y_goal);
-            elseif h_type == 'e'
+            elseif obj.h_type == 'e'
                 % euclidian distance
                 cost_h = sqrt((x_neig - x_goal)^2 + (y_neig - y_goal)^2);
             else
--- HW1/Astar/main_Astar_2D.m
+++ HW1/Astar/main_Astar_2D.m
@@ -9,12 +9,12 @@ map = read_map('maze2.pgm');
 % set start and goal info.
 [start, num_nodes] = get_start(map);
 goal = get_goal(map);
-h_type = "m"; % manhattan distance as heuristic
+h_type = "e"; % select type of heuristic "m" is manhattan "e" is euclidian
 
 @Astar.m;
-astar = Astar;
+astar = Astar(start, goal, map, h_type);
 
-path = astar.search(start, goal, map, h_type);
+path = astar.search();
 if h_type == 'm'
     plot_path(map, path, 'A* path planning result, Manhattan heuristic')
 else
