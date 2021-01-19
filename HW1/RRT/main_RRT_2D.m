f58cc044119c7fa08620072da136a96c8cc4adbb
Masafumi Endo <endoma@oregonstate.edu>
2021-01-19 14:25:18 -0800

    small modification

--- HW1/RRT/main_RRT_2D.m
+++ HW1/RRT/main_RRT_2D.m
@@ -1,5 +1,5 @@
 % Author: Masafumi Endo
-% Date: 01/14/2021
+% Date: 01/19/2021
 % Version: 1.0
 % Description: Main file to run RRT algorithm in 2D environment
 
@@ -11,7 +11,7 @@ map = read_map('maze2.pgm');
 goal = get_goal(map);
 dist = 1; % dist to steer sampled point
 sampling_rate = 0.2; % rate to sample goal point
-rand_range = [0, 25]; % range to sample random point
+rand_range = [0, 25]; % range to sample random point [min, max]
 
 @RRT.m;
 rrt = RRT(start, goal, map, dist, sampling_rate, rand_range);
