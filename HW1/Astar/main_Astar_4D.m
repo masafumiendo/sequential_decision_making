1db8839f9cdf7693e6ccc114c26a759316e76745
masafumiendo <endoma@oregonstate.edu>
2021-01-24 17:25:06 -0800

    add A* dynamic

--- HW1/Astar/main_Astar_4D.m
+++ HW1/Astar/main_Astar_4D.m
@@ -1,20 +1,20 @@
 % Author: Masafumi Endo
-% Date: 01/14/2021
+% Date: 01/24/2021
 % Version: 1.0
-% Description: Main file to run A* algorithm in 2D environment
+% Description: Main file to run A* algorithm in 4D environment
 
 % select maze to find path
-map = read_map('maze1.pgm');
+map = read_map_for_dynamics('maze2.pgm');
 
 % set start and goal info.
-[start, num_nodes] = get_start(map);
-goal = get_goal(map);
+[start, num_nodes] = get_start_dynamic(map);
+goal = get_goal_dynamic(map);
 h_type = "m"; % select type of heuristic "m" is manhattan "e" is euclidian
 
-@Astar.m;
-astar = Astar(start, goal, map, h_type);
+@Astar_dynamic.m;
+astar_dynamic = Astar_dynamic(start, goal, map, h_type);
 
-path = astar.search();
+path = astar_dynamic.search();
 if h_type == 'm'
     plot_path(map, path, 'A* path planning result, Manhattan heuristic')
 else
