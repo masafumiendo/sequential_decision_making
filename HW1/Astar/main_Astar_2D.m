6a1e9a71c6fb5301b50d74b355e872534831d39c
masafumiendo <endoma@oregonstate.edu>
2021-01-27 00:27:46 -0800

    change map

--- HW1/Astar/main_Astar_2D.m
+++ HW1/Astar/main_Astar_2D.m
@@ -4,7 +4,7 @@
 % Description: Main file to run A* algorithm in 2D environment
 
 % select maze to find path
-map = read_map('maze2.pgm');
+map = read_map('maze1.pgm');
 
 % set start and goal info.
 [start, num_nodes] = get_start(map);
