be936aca77ad95f9d5564f812b0dbe408239eb9c
masafumiendo <endoma@oregonstate.edu>
2021-02-13 23:08:08 -0800

    change function

--- HW2/GameFiles/GreedyNavigator.py
+++ HW2/GameFiles/GreedyNavigator.py
@@ -42,9 +42,7 @@ class GreedyNavigator:
         :return: direction
         """
 
-        # This loop shows how you can create a mask, an grid of 0s and 1s
-        # where 0s represent unexplored areas and 1s represent explored areas
-        # This mask is used by the world estimating network
+        # create a mask for estimation
         mask = np.zeros((28, 28))
         for x in range(0, 28):
             for y in range(0, 28):
@@ -52,67 +50,59 @@ class GreedyNavigator:
                     mask[y, x] = 1
         # creates an estimate of what the world looks like before moving
         map_prediction = self.uNet.runNetwork(map, mask)
+        # get entropy for the current state based on map_prediction
+        entropy_curr = self._get_entropy_curr(map_prediction)
+
         # initialize dictionary
-        dict_info_quality = {}
+        dict_info_gain = {}
 
         # add possible movements w/ init.
         location_curr = robot.getLoc()
         if location_curr[0] - 1 >= 0:
-            dict_info_quality['left'] = 0
+            dict_info_gain['left'] = 0
         if location_curr[0] + 1 <= 27:
-            dict_info_quality['right'] = 0
+            dict_info_gain['right'] = 0
         if location_curr[1] + 1 <= 27:
-            dict_info_quality['down'] = 0
+            dict_info_gain['down'] = 0
         if location_curr[1] - 1 >= 0:
-            dict_info_quality['up'] = 0
+            dict_info_gain['up'] = 0
 
-        # calculate info quality for possible movements
-        for direction in dict_info_quality:
-            dict_info_quality[direction] = self._calc_info_quality(location_curr, direction, mask, map, map_prediction)
+        # calculate info gain for possible movements
+        for direction in dict_info_gain:
+            location_next = self._get_next_location(location_curr, direction)
+            entropy_next = self._get_entropy_next(mask, map, map_prediction, location_next)
+            if entropy_curr == entropy_next:
+                dict_info_gain[direction] = np.nan
+            else:
+                dict_info_gain[direction] = entropy_curr - entropy_next
+
         # remove the direction where no new information can be obtained
-        dict_info_quality = {k: dict_info_quality[k] for k in dict_info_quality if not np.isnan(dict_info_quality[k])}
+        dict_info_gain = {k: dict_info_gain[k] for k in dict_info_gain if not np.isnan(dict_info_gain[k])}
         # determine direction that gains the maximal information
-        direction = max(dict_info_quality, key=dict_info_quality.get)
+        direction = max(dict_info_gain, key=dict_info_gain.get)
 
-        self._summarize_progress(location_curr, dict_info_quality, direction, map_gt, map, map_prediction)
+        self._summarize_progress(location_curr, dict_info_gain, direction, map_gt, map, map_prediction)
 
         return direction
 
-    def _calc_info_quality(self, location_curr, direction, mask, map, map_prediction):
-        """ Calculate information quality for the possible movement
-        Hint: Consider the values of the pixels in the predicted map.
-        :return: info_quality
+    def _get_entropy_curr(self, map_prediction):
+        """ Get entropy for the current state and estimated map
+        :return: entropy
         """
 
-        location_next = self._get_next_location(location_curr, direction)
-        info_quality = self._get_info(mask, map, map_prediction, location_next)
+        # get a guess of what "world" we are in
+        char = self.classNet.runNetwork(map_prediction)
+        # calculate probability
+        prob = self.softmax(torch.from_numpy(char).clone()).numpy()
+        # calculate entropy
+        entropy = self._calc_entropy(prob[0])
 
-        return info_quality
+        return entropy
 
-    def _get_next_location(self, location_curr, direction):
-        """ Get next location of the robot
-        :return: location_next
+    def _get_entropy_next(self, mask, map, map_prediction, location):
+        """ Get entropy for the next state and hallucinate map
+        :return: entropy
         """
-        xLoc = location_curr[0]
-        yLoc = location_curr[1]
-
-        if direction == 'left':
-            xLoc = xLoc - 1
-        elif direction == 'right':
-            xLoc = xLoc + 1
-        elif direction == 'down':
-            yLoc = yLoc + 1
-        elif direction == 'up':
-            yLoc = yLoc - 1
-        else:
-            raise ValueError(f"Robot received invalid direction: {direction}!")
-
-        location_next = (xLoc, yLoc)
-
-        return location_next
-
-    def _get_info(self, mask, map, map_prediction, location):
-
         map_ = np.copy(map)
         # update map with predicted pixel values
         for x in range(-1, 2):
@@ -136,24 +126,15 @@ class GreedyNavigator:
                     mask[y, x] = 1
         # creates an estimate of what the world looks like after moving
         map_hallucinate = self.uNet.runNetwork(map_, mask)
-        # get a guess of what "world" we are in before and after taking action
-        char_bef_action = self.classNet.runNetwork(map_prediction)
-        char_aft_action = self.classNet.runNetwork(map_hallucinate)
 
+        # get a guess of what "world" we are in
+        char = self.classNet.runNetwork(map_hallucinate)
         # calculate probability
-        prob_bef = self.softmax(torch.from_numpy(char_bef_action).clone()).numpy()
-        prob_aft = self.softmax(torch.from_numpy(char_aft_action).clone()).numpy()
-
+        prob = self.softmax(torch.from_numpy(char).clone()).numpy()
         # calculate entropy
-        entropy_bef = self._calc_entropy(prob_bef[0])
-        entropy_aft = self._calc_entropy(prob_aft[0])
-        # calculate information gain
-        if entropy_bef == entropy_aft:
-            info_quality = np.nan
-        else:
-            info_quality = entropy_bef - entropy_aft
+        entropy = self._calc_entropy(prob[0])
 
-        return info_quality
+        return entropy
 
     def _calc_entropy(self, prob):
         """ Calculate entropy from probabilities
@@ -165,10 +146,32 @@ class GreedyNavigator:
 
         return entropy
 
-    def _summarize_progress(self, location, dict_info_quality, direction, map_gt, map, map_prediction):
+    def _get_next_location(self, location_curr, direction):
+        """ Get next location of the robot
+        :return: location_next
+        """
+        xLoc = location_curr[0]
+        yLoc = location_curr[1]
+
+        if direction == 'left':
+            xLoc = xLoc - 1
+        elif direction == 'right':
+            xLoc = xLoc + 1
+        elif direction == 'down':
+            yLoc = yLoc + 1
+        elif direction == 'up':
+            yLoc = yLoc - 1
+        else:
+            raise ValueError(f"Robot received invalid direction: {direction}!")
+
+        location_next = (xLoc, yLoc)
+
+        return location_next
+
+    def _summarize_progress(self, location, dict_info_gain, direction, map_gt, map, map_prediction):
         print(" ")
         print(f"before one iteration of the game -> Robot at: ({location[0]}, {location[1]})")
-        print(dict_info_quality)
+        print(dict_info_gain)
         print("{0} is selected as a next robot direction".format(direction))
 
         char = self.classNet.runNetwork(map_prediction)
