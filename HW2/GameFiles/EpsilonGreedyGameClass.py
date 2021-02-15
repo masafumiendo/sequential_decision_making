a994363c8f8139f3e69779298521500b5d2ada17
masafumiendo <endoma@oregonstate.edu>
2021-02-14 20:10:51 -0800

    implement epsilon-greedy

---
+++ HW2/GameFiles/EpsilonGreedyGameClass.py
@@ -0,0 +1,73 @@
+"""
+Authors: Masafumi Endo
+Date: 02/03/2021
+Version: 1.0
+Objective: Modify "GameClass" from the original class
+"""
+
+import numpy as np
+import torch
+from GameClass import Game
+from networkFolder.functionList import WorldEstimatingNetwork, DigitClassificationNetwork
+
+
+class EpsilonGreedyGame(Game):
+    def __init__(self, truthMap, digit, navigator, robot, epsilon, decay, specified_prob):
+        super().__init__(truthMap, digit, navigator, robot)
+
+        self.specified_prob = specified_prob
+        self.flag_greedy = True
+        self.uNet = WorldEstimatingNetwork()
+        self.classNet = DigitClassificationNetwork()
+        self.softmax = torch.nn.Softmax(dim=1)
+        self.num_stuck = 0
+        self.epsilon = epsilon
+        self.decay = decay
+
+    def tick(self):
+        self.epsilon = self.epsilon * np.exp(- self.decay * self.iterations)
+        self.iterations += 1
+        # Generate an action for the robot
+        if self.flag_greedy:
+            # decayed epsilon-greedy
+            if self.epsilon > np.random.rand(1):
+                action = self.navigator.getActionRandom(self.robot)
+            else:
+                action, flag_stuck = self.navigator.getActionGreedy(self.robot, self.exploredMap)
+                if flag_stuck:
+                    self.num_stuck += 1
+        else:
+            action = self.navigator.getActionShortest(self.robot, self._goal)
+        # Move the robot
+        self.robot.move(action)
+        # Update the explored map based on robot position
+        self._updateMap(self.robot, self.exploredMap, self.truthMap)
+
+        # creates an estimate of what the world looks like after moving
+        mask = np.zeros((28, 28))
+        for x in range(0, 28):
+            for y in range(0, 28):
+                if self.exploredMap[y, x] != 128:
+                    mask[y, x] = 1
+        image = self.uNet.runNetwork(self.exploredMap, mask)
+        # creates an estimate of what world we are in
+        char = self.classNet.runNetwork(image)
+        estimated_digit = char.argmax()
+
+        # calculate probability to check condition
+        char_tensor = torch.from_numpy(char).clone()
+        prob_tensor = self.softmax(char_tensor)
+        prob = prob_tensor.numpy()
+        prob_max = prob[0][estimated_digit]
+
+        # check condition to end up greedy exploration
+        if estimated_digit == self._digit and prob_max >= self.specified_prob:
+            # end greedy exploration
+            self.flag_greedy = False
+
+        if self.robot.getLoc() == self._goal:
+            self.score += 100
+            return True
+        else:
+            self.score -= 1
+            return False
---
+++ HW2/GameFiles/main_epsilon_greedy.py
@@ -0,0 +1,97 @@
+"""
+Authors: Masafumi Endo
+Date: 02/13/2021
+Version: 2.0
+Objective: main function to run epsilon-greedy navigator
+"""
+
+import gzip
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from RobotClass import Robot
+from EpsilonGreedyGameClass import EpsilonGreedyGame
+from GreedyNavigator import GreedyNavigator
+from networkFolder.functionList import Map, WorldEstimatingNetwork, DigitClassificationNetwork
+
+def run_greedy(map, epsilon, decay, specified_prob):
+
+    # get map
+    data = map.map
+    # init robot and navigator
+    robot = Robot(0, 0)
+    path_x = np.array(robot.xLoc)
+    path_y = np.array(robot.yLoc)
+    navigator = GreedyNavigator()
+    # init game class w/ user-specified probability
+    game = EpsilonGreedyGame(data, map.number, navigator, robot, epsilon, decay, specified_prob)
+    # init networks
+    uNet = WorldEstimatingNetwork()
+    classNet = DigitClassificationNetwork()
+
+    # print the number of the current map
+    print(map.number)
+
+    # run game until the robot found the goal
+    while True:
+        found_goal = game.tick()
+        print(" ")
+        print(f"after one iteration of the game -> {game.getIteration()}: Robot at: ({robot.xLoc}, {robot.yLoc}), Score = {game.getScore()}")
+        # add path
+        path_x = np.append(path_x, robot.xLoc)
+        path_y = np.append(path_y, robot.yLoc)
+        if found_goal:
+            print(f"Found goal at time step: {game.getIteration()}!")
+            print(f"Number of stucked: {game.num_stuck}")
+            break
+    print(f"Final Score: {game.score}")
+
+    # create mask
+    mask = np.zeros((28, 28))
+    for x in range(0, 28):
+        for y in range(0, 28):
+            if game.exploredMap[y, x] != 128:
+                mask[y, x] = 1
+
+    # estimate what the world looks like
+    image = uNet.runNetwork(game.exploredMap, mask)
+    # estimate what "world" we are in
+    char = classNet.runNetwork(image)
+    # get the most likely digit
+    print(char.argmax())
+
+    # show ground truth, explored, and predicted map
+    # ground truth
+    fig, ax = plt.subplots()
+    ax.imshow(game.truthMap, cmap='gray')
+    plt.subplots_adjust(left=0.05, right=0.95, bottom=0.1, top=0.95)
+    plt.savefig('../fig/map_gt_' + str(map.number) + '_greedy.png')
+    plt.show()
+    # explored w/ a robot's trajectory
+    fig, ax = plt.subplots()
+    ax.plot(path_x, path_y, color='red')
+    ax.imshow(game.exploredMap, cmap='gray')
+    plt.subplots_adjust(left=0.05, right=0.95, bottom=0.1, top=0.95)
+    plt.savefig('../fig/map_explore_' + str(map.number) + '_greedy.png')
+    plt.show()
+    # predicted
+    fig, ax = plt.subplots()
+    ax.imshow(image, cmap='gray')
+    plt.subplots_adjust(left=0.05, right=0.95, bottom=0.1, top=0.95)
+    plt.savefig('../fig/map_pred_' + str(map.number) + '_greedy.png')
+    plt.show()
+
+if __name__ == '__main__':
+    # init map and prob
+    map = Map()
+    epsilon = 1
+    decay = 1/15
+    specified_prob = 0.95
+
+    # run exploration
+    run_greedy(map, epsilon, decay, specified_prob)
+
+    # run exploration
+    map.getNewMap()
+    map.getNewMap()
+    run_greedy(map, epsilon, decay, specified_prob)
