31fc6f299133aa2464b3e6768781445d26dcbab9
masafumiendo <endoma@oregonstate.edu>
2021-03-07 12:37:00 -0800

    add figs

--- HW3/implementation/main_qNavigate.m
+++ HW3/implementation/main_qNavigate.m
@@ -3,9 +3,9 @@
 % Objective: run qNavigate
 
 % set variable
-noise = 0.1;
+noise = 0.2;
 discount = 0.9;
-epsilon = 0.001;
+epsilon = 0.00001;
 % load maze environment
 maze = load_maze('maze0.txt');
 
